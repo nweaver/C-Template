9fd7419debe88e1173df801a0249677f5c4e5943
Nick Weaver <ncweaver@ucdavis.edu>
2024-09-16 14:54:27 +0000

    Skeleton

--- confuzzle.c
+++ confuzzle.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 static const uint8_t sbox[256] = {
   //0     1    2      3     4    5     6     7      8    9     A      B    C     D     E     F
